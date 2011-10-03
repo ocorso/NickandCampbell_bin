7532c4a232c763fe89b08a7062abb15b7d79fb63
Owen Corso <owen@ored.net>
2011-10-03 18:23:22 -0400

    testing auth.net API call

--- application/controllers/CheckoutController.php
+++ application/controllers/CheckoutController.php
@@ -6,11 +6,25 @@ class CheckoutController extends Zend_Controller_Action
     public function init()
     {
         /* Initialize action controller here */
-    	 require_once 'anet_php_sdk/AuthorizeNet.php'; 
-    }
+    }//end function
 
     public function indexAction()
     {
+    	print_r("hey there");
+    	require_once 'anet_php_sdk/AuthorizeNet.php'; 
+    	define("AUTHORIZENET_API_LOGIN_ID", "94EtqH8y");
+    	define("AUTHORIZENET_TRANSACTION_KEY", "64UyF8TFt7cUA22U");
+    	define("AUTHORIZENET_SANDBOX", true);
+    	$sale = new AuthorizeNetAIM;
+   		$sale->amount 		= "5.99";
+    	$sale->card_num 	= '6011000000000012';
+    	$sale->exp_date 	= '04/15';
+    	$response 			= $sale->authorizeAndCapture();
+    	if ($response->approved) {
+        	$transaction_id = $response->transaction_id;
+    	}//end if
+	    else echo "fail";
+    	
         $request	= $this->getRequest();
         $form		= new Application_Form_Checkout();
         
--- application/forms/Checkout.php
+++ application/forms/Checkout.php
@@ -64,7 +64,7 @@ class Application_Form_Checkout extends Zend_Form
 		));
 		
 		//city
-		$this->addElement('text','addr1',array(
+		$this->addElement('text','city',array(
 			'label'		=> 	'City:',
 			'required'	=>	false,
 			'filters'	=>	array('StringTrim'),
@@ -74,12 +74,12 @@ class Application_Form_Checkout extends Zend_Form
 		
 		));
 		
+		//states
 		$statesArr = array('AL'=>'Alabama','AK'=>'Alaska','AZ'=>'Arizona','AR'=>'Arkansas','CA'=>'California','CO'=>'Colorado','CT'=>'Connecticut','DE'=>'Delaware','DC'=>'District Of Columbia','FL'=>'Florida','GA'=>'Georgia','HI'=>'Hawaii','ID'=>'Idaho','IL'=>'Illinois', 'IN'=>'Indiana', 'IA'=>'Iowa',  'KS'=>'Kansas','KY'=>'Kentucky','LA'=>'Louisiana','ME'=>'Maine','MD'=>'Maryland', 'MA'=>'Massachusetts','MI'=>'Michigan','MN'=>'Minnesota','MS'=>'Mississippi','MO'=>'Missouri','MT'=>'Montana','NE'=>'Nebraska','NV'=>'Nevada','NH'=>'New Hampshire','NJ'=>'New Jersey','NM'=>'New Mexico','NY'=>'New York','NC'=>'North Carolina','ND'=>'North Dakota','OH'=>'Ohio','OK'=>'Oklahoma', 'OR'=>'Oregon','PA'=>'Pennsylvania','RI'=>'Rhode Island','SC'=>'South Carolina','SD'=>'South Dakota','TN'=>'Tennessee','TX'=>'Texas','UT'=>'Utah','VT'=>'Vermont','VA'=>'Virginia','WA'=>'Washington','WV'=>'West Virginia','WI'=>'Wisconsin','WY'=>'Wyoming'); 
 		$state = new Zend_Form_Element_Select('state');
 		$state->setLabel('State')
 		->setMultiOptions($statesArr)
 		->setRegisterInArrayValidator(false);
-		
 		$this->addElement($state);
 		
 		//zip
