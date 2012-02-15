90dc31852a174e8b0329bb4eae0681f3e5675e6f
Owen Corso <owen@ored.net>
2012-02-15 18:33:09 -0500

    updated jquery and finally figured out env switching, like a boss

--- public/index.php
+++ public/index.php
@@ -5,8 +5,13 @@ defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
 // Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+if (!defined('APPLICATION_ENV')){
+	switch ($_SERVER["HTTP_HOST"]){
+		case "staging.nickandcampbell.com": define('APPLICATION_ENV','staging'); break;
+		case "nc.dev": define('APPLICATION_ENV','development'); break;
+		default : define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+	}
+}
 
 // Ensure library/ is on include_path
 set_include_path(implode(PATH_SEPARATOR, array(
