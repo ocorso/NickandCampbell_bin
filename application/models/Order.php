5f2d10d45994648f6fb1f258252294ca6d52f976
O Red <owen@ored.net>
2012-03-13 23:35:30 -0400

    fleshing out order model

--- application/models/Order.php
+++ application/models/Order.php
@@ -7,6 +7,7 @@ class Application_Model_Order
 	protected $_total_price;
 	protected $_total_weight;
 	protected $_total_tax;
+	protected $_ref_cart_id;
 	protected $_ref_uid;
 	protected $_ref_bid;
 	protected $_ref_shid;
@@ -61,9 +62,15 @@ class Application_Model_Order
 			 		'total_price'		=> $this->_total_price,
 					'total_weight'		=> $this->_total_weight,
 					'total_tax'			=> $this->_ref_pid,
+					'ref_cart_id'		=> $this->_ref_cart_id,
 					'ref_uid'			=> $this->_ref_uid,
 				    'ref_shid'			=> $this->_ref_shid,
-				    'created_at'		=> $this->_created_at
+				    'ref_shipping_type'	=> $this->_ref_shipping_type,
+				    'shipping_date'		=> $this->_shipping_date,
+				    'shipping_price'	=> $this->_shipping_price,
+				    'tracking_num'		=> $this->_tracking_num,
+				    'created_at'		=> $this->_created_at,
+				    'status'			=> $this->_status
 				   );
 		return $a;
 	}
@@ -85,5 +92,36 @@ class Application_Model_Order
 	
 	public function setTotal_tax($d){ $this->_total_tax = $d; return $this; }
 	public function getTotal_tax(){ return $this->_total_tax; }
+	
+	public function setCart_id($d){ $this->_ref_cart_id = $d; return $this; }
+	public function getCart_id(){ return $this->_ref_cart_id; }
+	
+	public function setRef_uid($d){ $this->_ref_uid = $d; return $this; }
+	public function getRef_uid(){ return $this->_ref_uid; }
+	
+	public function setRef_bid($d){ $this->_ref_bid = $d; return $this; }
+	public function getRef_bid(){ return $this->_ref_uid; }
+	
+	public function setRef_shid($d){ $this->_ref_shid = $d; return $this; }
+	public function getRef_shid(){ return $this; }
+
+	public function setRef_shipping_type($d){ $this->_ref_shipping_type = $d; return $this; }
+	public function getRef_shipping_type(){ return $this->_ref_shipping_type; }
+	
+	public function setShipping_date($d){ $this->_shipping_date = $d; return $this; }
+	public function getShipping_date(){ return $this->_shipping_date; }
+
+	public function setShipping_price($d){ $this->_shipping_price = $d; return $this; }
+	public function getShipping_price(){ return $this->_shipping_price; }
+	
+	public function setTracking_num($d){ $this->_tracking_num = $d; return $this; }
+	public function getTracking_num(){ return $this->_tracking_num; }
+	
+	public function setCreated_At($d){ $this->_created_at = $d; return $this; }
+	public function getCreated_At(){ return $this->_created_at; }
+	
+	public function setStatus($d){ $this->_status = $d; return $this; }
+	public function getStatus(){ return $this->_status; }
+
 }
 
--- application/models/PostOrderCart.php
+++ application/models/PostOrderCart.php
@@ -2,7 +2,99 @@
 
 class Application_Model_PostOrderCart
 {
+	protected $_cart_id;
+	protected $_type;
+	protected $_promo;
+	protected $_ref_pid;
+	protected $_ref_uid;
+	protected $_quantity;
+	protected $_created_at;
 
-
-}
+	// =================================================
+	// ================ Workers
+	// =================================================
+	public function __set($name, $value){
+	
+		$method = 'set'.$name;
+	
+		if(('mapper' == $name)|| !method_exists($this, $method)){
+			throw new Exception($name.' is an invalid postorder_cart property');
+		}//end if
+	
+		$this->$method($value);
+	
+	}//end function
+	
+	public function __get($name){
+	
+		$method = 'get'.$name;
+	
+		if(('mapper' == $name)|| !method_exists($this, $method)){
+			throw new Exception($name.' is an invalid postorder_cart property');
+		}//end if
+			
+		return $this->$method();
+	
+	}//end function
+	
+	public function setOptions(array $options){
+		
+		$methods = get_class_methods($this);
+		
+		foreach($options as $key => $value){
+			$method = 'set' . ucfirst($key);
+			if (in_array($method, $methods))	$this->$method($value);
+		}// endforeach
+	
+	}//end function
+	
+	public function toArray(){
+		$a = array( 'id'		=> $this->_id,
+					'sesh_id'	=> $this->_sesh_id,
+			 		'type'		=> $this->_type,
+					'promo'		=> $this->_promo,
+					'ref_pid'	=> $this->_ref_pid,
+					'ref_uid'	=> $this->_ref_uid,
+				    'quantity'	=> $this->_quantity,
+				    'created_at'=> $this->_created_at
+				   );
+		return $a;
+	}
+	
+	// =================================================
+	// ================ Getters / Setters
+	// =================================================
+	public function setCart_id($id){ $this->_cart_id = $id; return $this; }
+	public function getCart_id(){ return $this->_cart_id; }
+	
+	public function getType(){ return $this->_type; }
+	public function setType($d){ $this->_type = $d; return $this;}
+	
+	public function getPromo(){ return $this->_promo;}
+	public function setPromo($d){ $this->_promo = $d; return $this;}
+	
+	public function getRef_pid(){ return $this->_ref_pid;}
+	public function setRef_pid($d){ $this->_ref_pid = $d; return $this;}
+	
+	public function getRef_uid(){ return $this->_ref_uid;}
+	public function setRef_uid($d){ $this->_ref_uid = $d; return $this;}
+	
+	public function getQuantity(){ return $this->_quantity;}
+	public function setQuantity($d){ $this->_quantity = $d; return $this;}
+	
+	public function getCreated_at(){ return $this->_created_at;}
+	public function setCreated_at($d){	$this->_created_at	= $d; return $this;}
+	
+	// =================================================
+	// ================ Constructor
+	// =================================================
+	
+	public function __construct(array $options = null){
+	
+		if (is_array($options)){
+			$this->setOptions($options);
+		}//end if
+	}//end constructor
+	
+}//end class
 
